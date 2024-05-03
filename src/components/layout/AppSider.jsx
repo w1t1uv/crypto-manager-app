3f1f5fa3a6179ec1bbff014d368b15268d5c176d
Egor Ushakov <witluv@icloud.com>
2024-05-03 16:20:06 +0300

    feat: add card rendering to the sidebar

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,16 +1,77 @@
-import { Layout } from 'antd';
+import { useEffect, useState } from 'react';
+import { Card, Layout, List, Statistic, Typography, Spin } from 'antd';
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
+import { fakeFetchCrypto, fetchAssets } from '../../api';
+import { percentDifference } from '../../utils';
 
 const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
+  padding: '1rem',
 };
 
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
 export default function AppSider() {
+  const [loading, setLoading] = useState(false);
+  const [crypto, setCrypto] = useState([]);
+  const [assets, setAssets] = useState([]);
+
+  useEffect(() => {
+    async function preload() {
+      setLoading(true);
+      const { result } = await fakeFetchCrypto();
+      const assets = await fetchAssets();
+
+      setCrypto(result);
+      setAssets(
+        assets.map((asset) => {
+          const coin = result.find((c) => c.id === asset.id);
+          return {
+            grow: asset.price < coin.price,
+            growPercent: percentDifference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            ...asset,
+          };
+        })
+      );
+      setLoading(false);
+    }
+    preload();
+  }, []);
+
+  if (loading) {
+    return <Spin fullscreen />;
+  }
+
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-      Sider
+      {assets.map((asset) => (
+        <Card key={asset.id} style={{ marginBottom: '1rem' }}>
+          <Statistic
+            title={asset.id}
+            value={asset.totalAmount}
+            precision={2}
+            valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322' }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix="$"
+          />
+          <List
+            size="small"
+            dataSource={data}
+            renderItem={(item) => (
+              <List.Item>
+                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+              </List.Item>
+            )}
+          />
+        </Card>
+      ))}
     </Layout.Sider>
   );
 }
