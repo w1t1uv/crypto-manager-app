1d344a4924a1edc2e80ae0b4a2f4c56d46546b4c
Egor Ushakov <witluv@icloud.com>
2024-05-03 16:29:11 +0300

    feat: add more styles for a better user experience

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,20 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Card, Layout, List, Statistic, Typography, Spin } from 'antd';
+import { Card, Layout, List, Statistic, Spin, Typography, Tag } from 'antd';
 import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import { fakeFetchCrypto, fetchAssets } from '../../api';
-import { percentDifference } from '../../utils';
+import { capitalize, percentDifference } from '../../utils';
 
 const siderStyle = {
   padding: '1rem',
 };
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 
 export default function AppSider() {
   const [loading, setLoading] = useState(false);
@@ -54,7 +46,7 @@ export default function AppSider() {
       {assets.map((asset) => (
         <Card key={asset.id} style={{ marginBottom: '1rem' }}>
           <Statistic
-            title={asset.id}
+            title={capitalize(asset.id)}
             value={asset.totalAmount}
             precision={2}
             valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322' }}
@@ -63,10 +55,30 @@ export default function AppSider() {
           />
           <List
             size="small"
-            dataSource={data}
+            dataSource={[
+              {
+                title: 'Total Profit',
+                value: asset.totalProfit,
+                withTag: true,
+              },
+              { title: 'Asset Amount', value: asset.amount, isPlain: true },
+            ]}
             renderItem={(item) => (
               <List.Item>
-                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                <span>{item.title}</span>
+                <span>
+                  {item.withTag && (
+                    <Tag color={asset.grow ? 'green' : 'red'}>
+                      {asset.growPercent}
+                    </Tag>
+                  )}
+                  {item.isPlain && item.value}
+                  {!item.isPlain && (
+                    <Typography.Text type={asset.grow ? 'success' : 'danger'}>
+                      {item.value.toFixed(2)}$
+                    </Typography.Text>
+                  )}
+                </span>
               </List.Item>
             )}
           />
