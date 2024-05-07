845fc93eb3e8af88ece19b0b62c23a08520a0b1f
Egor Ushakov <witluv@icloud.com>
2024-05-07 20:01:36 +0300

    feat: add coin selection and add button to header

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,14 +1,38 @@
-import { Layout } from 'antd';
+import { Button, Layout, Select, Space } from 'antd';
+import { useCrypto } from '../../context/crypto-context.jsx';
 
 const headerStyle = {
+  width: '100%',
   textAlign: 'center',
-  color: '#fff',
+  padding: '1rem',
+  display: 'flex',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 };
 
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>;
+  const { crypto } = useCrypto();
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{ width: 250 }}
+        value="press / to open"
+        optionLabelProp="label"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img src={option.data.icon} alt={option.data.label} />{' '}
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button type="primary">Add Asset</Button>
+    </Layout.Header>
+  );
 }
