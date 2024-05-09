3b72e4f2bf19cf74a9903aec4785747ab09f7ee1
Egor Ushakov <witluv@icloud.com>
2024-05-09 23:05:36 +0300

    feat: add fields for entering information about the purchased coin when adding it to the portfolio

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,3 +1,85 @@
+import { useState } from 'react';
+import { Select, Space, Form, InputNumber, Button, DatePicker } from 'antd';
+import { useCrypto } from '../context/crypto-context';
+
 export default function AddAssetForm() {
-  return <h2>Form Asset</h2>;
+  const { crypto } = useCrypto();
+  const [coin, setCoin] = useState();
+
+  if (!coin) {
+    return (
+      <Select
+        style={{ width: '100%' }}
+        onSelect={(v) => setCoin(crypto.find((c) => c.id === v))}
+        placeholder="Select coin"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{ width: 20 }}
+              src={option.data.icon}
+              alt={option.data.label}
+            />{' '}
+            {option.data.label}
+          </Space>
+        )}
+      />
+    );
+  }
+
+  function onFinish(values) {
+    console.log('finish', values);
+  }
+
+  function onFinishFailed(values) {
+    console.log('onFinishFailed', values);
+  }
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 10 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{}}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Amount"
+        name="amount"
+        rules={[
+          {
+            required: true,
+            type: 'number',
+            min: 0,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <InputNumber style={{ width: '100%' }} />
+      </Form.Item>
+
+      <Form.Item label="Price" name="price">
+        <InputNumber disabled style={{ width: '100%' }} />
+      </Form.Item>
+
+      <Form.Item label="Date & Time" name="date">
+        <DatePicker showtime />
+      </Form.Item>
+
+      <Form.Item label="Total" name="total">
+        <InputNumber disabled style={{ width: '100%' }} />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Add Asset
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
