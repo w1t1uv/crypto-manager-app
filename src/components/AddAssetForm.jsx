efd23a15f0a93291dba11c56a16509e012f6f3dc
Egor Ushakov <witluv@icloud.com>
2024-05-10 13:55:22 +0300

    feat: add dynamic change of the total cost when the quantity changes

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -2,7 +2,18 @@ import { useState } from 'react';
 import { Select, Space, Form, InputNumber, Button, DatePicker } from 'antd';
 import { useCrypto } from '../context/crypto-context';
 
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    number: '${label} is not valid number',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
 export default function AddAssetForm() {
+  const [form] = Form.useForm();
   const { crypto } = useCrypto();
   const [coin, setCoin] = useState();
 
@@ -39,14 +50,25 @@ export default function AddAssetForm() {
     console.log('onFinishFailed', values);
   }
 
+  function handleAmountChange(value) {
+    const price = form.getFieldValue('price');
+    form.setFieldsValue({
+      total: +(value * price).toFixed(2),
+    });
+  }
+
   return (
     <Form
+      form={form}
       name="basic"
       labelCol={{ span: 4 }}
       wrapperCol={{ span: 10 }}
       style={{ maxWidth: 600 }}
-      initialValues={{}}
+      initialValues={{
+        price: +coin.price.toFixed(2),
+      }}
       onFinish={onFinish}
+      validateMessages={validateMessages}
     >
       <Form.Item
         label="Amount"
@@ -56,11 +78,14 @@ export default function AddAssetForm() {
             required: true,
             type: 'number',
             min: 0,
-            message: 'Please input your username!',
           },
         ]}
       >
-        <InputNumber style={{ width: '100%' }} />
+        <InputNumber
+          placeholder="Enter coin amount"
+          onChange={handleAmountChange}
+          style={{ width: '100%' }}
+        />
       </Form.Item>
 
       <Form.Item label="Price" name="price">
