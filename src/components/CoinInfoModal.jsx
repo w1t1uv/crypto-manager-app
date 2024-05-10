6f91ee72f1744047af381bc194cc946e34dc8699
Egor Ushakov <witluv@icloud.com>
2024-05-10 19:26:44 +0300

    feat: move the coin information display to a separate component

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,18 +1,10 @@
-import { Divider, Flex, Tag, Typography } from 'antd';
+import { Divider, Tag, Typography } from 'antd';
+import CoinInfo from './layout/CoinInfo.jsx';
 
 export default function CoinInfoModal({ coin }) {
   return (
     <>
-      <Flex align="center">
-        <img
-          src={coin.icon}
-          alt={coin.name}
-          style={{ width: 40, marginRight: 10 }}
-        />
-        <Typography.Title level={2} style={{ margin: 0 }}>
-          ({coin.symbol}) {coin.name}
-        </Typography.Title>
-      </Flex>
+      <CoinInfo coin={coin} withSymbol />
       <Divider />
       <Typography.Paragraph>
         <Typography.Text strong>1 hour: </Typography.Text>
